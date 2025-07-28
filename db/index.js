433892ef3249cc59571ce6a1dfa2e4408b6fb81c
Harsh Chauhan <harshchauhan10may2004@gmail.com>
2025-07-28 16:50:46 +0530

    succesful database connection

--- db/index.js
+++ db/index.js
@@ -4,10 +4,8 @@ import {DBName} from "./../src/constant.js";
 
 const connectDB=async ()=>{
     try{
+        console.log(`${process.env.DATABASECONNECT}/${DBName}`);
         const databaseInstance=await mongoose.connect(`${process.env.DatabaseConnect}/${DBName}`);
-        console.log(databaseInstance);
-        console.log("database connected");
-        console.log(databaseInstance.connection.host);
     }
     catch(error){
         console.log(`error occured ${error}`);
