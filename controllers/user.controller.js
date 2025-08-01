4880a30f1b3a866a53e5883502210fbe8710ea35
Harsh Chauhan <harshchauhan10may2004@gmail.com>
2025-08-01 20:03:02 +0530

    watch hisory

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -264,11 +264,59 @@ const getUserChannelProfile=tryCatch(async (req,res)=>{
         new ApiResponse(200,channel[0],"channel info send succesfully")
     )
 })
+
+const getWatchHistory=tryCatch(async (req,res)=>{
+    const user=User.aggregate([
+        {$match:{
+            _id: new mongoose.Schema.Types.ObjectId(user._id)
+        }},
+        {$lookup:{
+            from:"videos",
+            localField:"watchHistory",
+            foreignField:"_id",
+            as:"watchHistory",
+            pipeline:[
+                {
+                    $lookup:{
+                        from:"users",
+                        localField:"owner",
+                        foreignField:"_id",
+                        as:"owner",
+                        pipeline:[
+                            {
+                            $project:{
+                                fullName:1,
+                                username:1,
+                                avatar:1
+                            }
+                        }
+                        ]
+                    }
+                },
+                {
+                    $addField:{
+                        owner:{
+                            $first:"$owner"
+                        }
+                    }
+                }
+            ]
+            
+        }}
+    ])
+    return res.
+    status(200).
+    json(
+        new ApiResponse(200,user[0].WatchHistory,"watch history fetched succesfully")
+    )
+});
 export {
     registerUser,
     loginUser,
     logoutUser,
     refreshAccessToken,
     changePassword,
-    updateAvatar
+    updateAvatar,
+    getWatchHistory,
+    getUserChannelProfile
 };
