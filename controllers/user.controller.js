b3dce87c0aac264496ae4b6d54de8d402a1796ec
Harsh Chauhan <harshchauhan10may2004@gmail.com>
2025-08-01 22:09:15 +0530

    completed

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -104,8 +104,8 @@ const logoutUser=tryCatch(async(req,res)=>{
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set:{
-                refreshToken:undefined
+            $unset:{
+                refreshToken:1
             }
         },{
             new:true
